43728db284d3216350cc2b2c7a368220142c6ddb
eviler007 <eviler_zhao@163.com>
2021-03-20 03:06:14 +0800

    update

--- server/index.js
+++ server/index.js
@@ -1,8 +1,5 @@
 const history = require('connect-history-api-fallback');
-const connect = require('connect');
-const path = require('path');
+const express = require('express');
 
-const app = connect();
-app.use(history());
-console.log('path.join(__dirname + /dist)', path.join(__dirname + '/dist'));
-app.use(path.join(__dirname + '/dist'));
+const app = express();
+app.use('/', history());
