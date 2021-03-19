713e315e511f13b04b2c163e107860888adae35d
eviler007 <eviler_zhao@163.com>
2021-03-20 02:38:47 +0800

    update

--- server/index.js
+++ server/index.js
@@ -1,33 +1,6 @@
-const http = require('http');
+const history = require('connect-history-api-fallback');
+const connect = require('connect');
 const httpPort = 80;
 
-const proxy = http.createServer((req, res) => {
-  res.end();
-});
-proxy.listen(httpPort, () => {
-  console.log('Server listening on: http://localhost:%s', httpPort);
-  const options = {
-    port: httpPort,
-    host: 'localhost',
-    method: 'CONNECT',
-    path: 'localhost:' + httpPort
-  };
-  const req = http.request(options);
-  req.end();
-
-  req.on('connect', (res, socket) => {
-    console.log('已连接');
-
-    // 通过 HTTP 隧道发出请求。
-    socket.write('GET / HTTP/1.1\r\n' +
-                 'Host: nodejs.cn:80\r\n' +
-                 'Connection: close\r\n' +
-                 '\r\n');
-    socket.on('data', (chunk) => {
-      console.log(chunk.toString());
-    });
-    socket.on('end', () => {
-      proxy.close();
-    });
-  });
-});
+const app = connect();
+app.use(history()).listen(httpPort);
