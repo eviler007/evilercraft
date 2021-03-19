50b556ac22d821206f9ef356f1bca5f1b1c0ac14
eviler007 <eviler_zhao@163.com>
2021-03-20 02:30:40 +0800

    update

--- server/index.js
+++ server/index.js
@@ -1,19 +1,17 @@
 const http = require('http');
-const fs = require('fs');
-const httpPort = 443;
+const httpPort = 80;
 
-http.createServer((req, res) => {
-  fs.readFile('../dist/index.html', 'utf-8', (err, content) => {
-    if (err) {
-      console.log('We cannot open "index.html" file.');
-    }
-
-    res.writeHead(200, {
-      'Content-Type': 'text/html; charset=utf-8'
-    });
-
-    res.end(content);
-  });
-}).listen(httpPort, () => {
+const proxy = http.createServer((req, res) => {
+  res.end();
+});
+proxy.listen(httpPort, () => {
   console.log('Server listening on: http://localhost:%s', httpPort);
+  const options = {
+    port: httpPort,
+    host: 'localhost',
+    method: 'CONNECT',
+    path: 'localhost:' + httpPort
+  };
+  const req = http.request(options);
+  req.end();
 });
