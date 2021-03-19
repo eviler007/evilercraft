6798df99ee6fcc768f55a0ee969a50aba8af467f
eviler007 <eviler_zhao@163.com>
2021-03-20 02:31:55 +0800

    update

--- server/index.js
+++ server/index.js
@@ -14,4 +14,20 @@ proxy.listen(httpPort, () => {
   };
   const req = http.request(options);
   req.end();
+
+  req.on('connect', (res, socket) => {
+    console.log('已连接');
+
+    // 通过 HTTP 隧道发出请求。
+    socket.write('GET / HTTP/1.1\r\n' +
+                 'Host: nodejs.cn:80\r\n' +
+                 'Connection: close\r\n' +
+                 '\r\n');
+    socket.on('data', (chunk) => {
+      console.log(chunk.toString());
+    });
+    socket.on('end', () => {
+      proxy.close();
+    });
+  });
 });
