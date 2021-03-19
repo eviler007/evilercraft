1bf75dd0af15ad9c7ad73c3c87fe2780cbbb848e
eviler007 <eviler_zhao@163.com>
2021-03-20 01:25:41 +0800

    add route

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,20 @@
+const path = require('path');
+const PrerenderSPAPlugin = require('prerender-spa-plugin');
 const WebpackBundleAnalyzer = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
   publicPath: './',
   chainWebpack: config => {
     if (process.env.NODE_ENV === 'production') {
+      // 包模块分析工具
       config.plugin('bundle-analyzer')
         .use(WebpackBundleAnalyzer, []);
+      // 预渲染
+      config.plugin('prerender-spa')
+        .use(PrerenderSPAPlugin, [{
+          staticDir: path.join(__dirname, 'dist'),
+          routes: [ '/', '/about' ]
+        }]);
     }
   }
 };
