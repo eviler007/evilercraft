8fc0c756273eff68246084ec5c7895a880c75149
eviler007 <eviler_zhao@163.com>
2021-03-20 01:51:46 +0800

    update

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const fs = require('fs');
-const httpPort = 80;
+const httpPort = 443;
 
 http.createServer((req, res) => {
   fs.readFile('index.html', 'utf-8', (err, content) => {
