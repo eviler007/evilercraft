cbbdfed924f4d590705d4c535fbb9251d62f8848
eviler007 <eviler_zhao@163.com>
2021-03-20 14:18:19 +0800

    update

--- vue.config.js
+++ vue.config.js
@@ -12,8 +12,21 @@ module.exports = {
       // 预渲染
       config.plugin('prerender-spa')
         .use(PrerenderSPAPlugin, [{
-          staticDir: path.join(__dirname, './dist'),
-          routes: [ '/', '/about' ]
+          staticDir: path.join(__dirname, 'dist'),
+          routes: [ '/', '/about' ],
+          postProcess (renderedRoute) {
+            // Ignore any redirects.
+            renderedRoute.route = renderedRoute.originalRoute;
+            // Basic whitespace removal. (Don't use this in production.)
+            renderedRoute.html = renderedRoute.html.split(/>[\s]+</gmi).join('><');
+            // Remove /index.html from the output path if the dir name ends with a .html file extension.
+            // For example: /dist/dir/special.html/index.html -> /dist/dir/special.html
+            if (renderedRoute.route.endsWith('.html')) {
+              renderedRoute.outputPath = path.join(__dirname, 'dist', renderedRoute.route);
+            }
+    
+            return renderedRoute;
+          },
         }]);
     }
   }
